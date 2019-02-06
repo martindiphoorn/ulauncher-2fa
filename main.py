f7fe6198fea5d4aad1febf7d69969a7f952c2f47
Martin Diphoorn <martin@diphoorn.com>
2019-02-06 12:46:37 +0100

    Show leading zero

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 
 import onetimepass as otp
+
 
 class TfaExtension(Extension):
 
     def __init__(self):
         super(TfaExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
 
 class KeywordQueryEventListener(EventListener):
 
@@ -30,14 +31,15 @@ class KeywordQueryEventListener(EventListener):
 
             secrets = provider.split("=")
             name = secrets[0]
-            token = otp.get_totp(secrets[1])
+            token = str(otp.get_totp(secrets[1])).zfill(6)
 
             items.append(ExtensionResultItem(icon='images/icon.png',
-                                     name='%s' % token,
-                                     description='Provider %s' % name,
-                                     on_enter=CopyToClipboardAction(str(token).zfill(6))))
+                                             name='%s' % token,
+                                             description='Provider %s' % name,
+                                             on_enter=CopyToClipboardAction(token)))
 
         return RenderResultListAction(items)
+
 
 if __name__ == '__main__':
     TfaExtension().run()
